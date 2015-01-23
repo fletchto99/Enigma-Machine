f858242d4bfadc963fa02092c1955ce34a4ac984
Matthew Langlois <fletchto99@gmail.com>
2015-01-23 17:00:22 -0500

    Other rotor constants

--- src/me/matt/enigma/EnigmaSettings.java
+++ src/me/matt/enigma/EnigmaSettings.java
@@ -11,21 +11,21 @@ public class EnigmaSettings {
     public static char[] ROTOR_III = new char[] { 'B', 'D', 'F', 'H', 'J', 'L',
             'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G',
             'A', 'K', 'M', 'U', 'S', 'Q', 'O' };
-    char[] ROTOR_IV = new char[] { 'E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y',
-            'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D',
-            'C', 'M', 'W', 'B' };
-    char[] ROTOR_V = new char[] { 'V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U',
-            'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O',
-            'F', 'E', 'C', 'K' };
-    char[] ROTOR_VI = new char[] { 'J', 'P', 'G', 'V', 'O', 'U', 'M', 'F', 'Y',
-            'Q', 'B', 'E', 'N', 'H', 'Z', 'R', 'D', 'K', 'A', 'S', 'X', 'L',
-            'I', 'C', 'T', 'W' };
-    char[] ROTOR_VII = new char[] { 'N', 'Z', 'J', 'H', 'G', 'R', 'C', 'X',
-            'M', 'Y', 'S', 'W', 'B', 'O', 'U', 'F', 'A', 'I', 'V', 'L', 'P',
-            'E', 'K', 'Q', 'D', 'T' };
-    char[] ROTOR_VIII = new char[] { 'N', 'Z', 'J', 'H', 'G', 'R', 'C', 'X',
-            'M', 'Y', 'S', 'W', 'B', 'O', 'U', 'F', 'A', 'I', 'V', 'L', 'P',
-            'E', 'K', 'Q', 'D', 'T' };
+    public static char[] ROTOR_IV = new char[] { 'E', 'S', 'O', 'V', 'P', 'Z',
+            'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T',
+            'G', 'K', 'D', 'C', 'M', 'W', 'B' };
+    public static char[] ROTOR_V = new char[] { 'V', 'Z', 'B', 'R', 'G', 'I',
+            'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M',
+            'J', 'Q', 'O', 'F', 'E', 'C', 'K' };
+    public static char[] ROTOR_VI = new char[] { 'J', 'P', 'G', 'V', 'O', 'U',
+            'M', 'F', 'Y', 'Q', 'B', 'E', 'N', 'H', 'Z', 'R', 'D', 'K', 'A',
+            'S', 'X', 'L', 'I', 'C', 'T', 'W' };
+    public static char[] ROTOR_VII = new char[] { 'N', 'Z', 'J', 'H', 'G', 'R',
+            'C', 'X', 'M', 'Y', 'S', 'W', 'B', 'O', 'U', 'F', 'A', 'I', 'V',
+            'L', 'P', 'E', 'K', 'Q', 'D', 'T' };
+    public static char[] ROTOR_VIII = new char[] { 'N', 'Z', 'J', 'H', 'G',
+            'R', 'C', 'X', 'M', 'Y', 'S', 'W', 'B', 'O', 'U', 'F', 'A', 'I',
+            'V', 'L', 'P', 'E', 'K', 'Q', 'D', 'T' };
 
     public static char[][] REFLECTOR_B = new char[][] {
             { 'A', 'Y', 'B', 'R', 'C', 'D', 'E', 'F', 'G', 'I', 'J', 'K', 'M',
